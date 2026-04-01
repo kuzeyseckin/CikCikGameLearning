e3ff2360b529618976e5ae54df58287e020d2257
Kuzey Seçkin <kuzeysee@hotmail.com>
2026-04-01 11:50:21 +0300

    Character Movement Refactoring  and SetState Setup

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
@@ -20,6 +20,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] private KeyCode _jumpKey;
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _jumpCooldown;
+    [SerializeField] private float _airMultiplier;
+    [SerializeField] private float _airDrag;
     [SerializeField] private bool _canJump;
     
     [Header("Ground Check Settings")]
@@ -27,6 +29,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private float _groundDrag;
     
+    private StateController _stateController;
     private Rigidbody _playerRigidbody;
     private float _horizontalInput, _verticalInput;
     private Vector3 _movementDirection;
@@ -57,33 +60,56 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
     }
 
+    private void SetStates()
+    {
+        var movementDirection = GetMovementDirection();
+        var isGrounded = IsGrounded();
+        var isSliding = IsSliding();
+        var currentState = _stateController.GetCurrentState();
+
+        var newState = currentState switch
+        {
+            _ when movementDirection == Vector3.zero && isGrounded && !isSliding => PlayerState.Idle,
+            _ when movementDirection != Vector3.zero && isGrounded && !isSliding => PlayerState.Move,
+            _ when movementDirection != Vector3.zero && isGrounded && isSliding => PlayerState.Slide,
+            _ when movementDirection == Vector3.zero && isGrounded && isSliding => PlayerState.SlideIdle,
+            _ when !_canJump && !isGrounded => PlayerState.Jump,
+            _ => currentState
+        };
+
+        if (newState != currentState)
+        {
+            _stateController.ChangeState(newState);
+        }
+        
+    }
+
     private void SetPlayerMovement()
     {
         _movementDirection = _orientationTransform.forward * _verticalInput
                              + _orientationTransform.right * _horizontalInput;
-       
+
+        float forceMultiplier = _stateController.GetCurrentState() switch
+        {
+            PlayerState.Move => 1f,
+            PlayerState.Slide => _slideMultiplier,
+            PlayerState.Jump => _airMultiplier,
+            _ => 1f
+        };
         
-        if (_isSliding == true)
-        {
-            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * _slideMultiplier, ForceMode.Force);
-        }
-        else
-        {
-            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
-        }
-       
+        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * forceMultiplier, ForceMode.Force);
+        
     }
 
     private void SetPlayerDrag()
     {
-        if (_isSliding)
+        _playerRigidbody.linearDamping = _stateController.GetCurrentState() switch
         {
-            _playerRigidbody.linearDamping = _slideDrag;
-        }
-        else
-        {
-            _playerRigidbody.linearDamping = _groundDrag;
-        }
+            PlayerState.Move => _groundDrag,
+            PlayerState.Slide => _slideDrag,
+            PlayerState.Jump => _airDrag,
+            _ => _playerRigidbody.linearDamping
+        };
         
     }
 
@@ -114,8 +140,19 @@ public class NewMonoBehaviourScript : MonoBehaviour
         return Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _groundLayer);
     }
 
+    private bool IsSliding()
+    {
+        return _isSliding;
+    }
+    
+    private Vector3 GetMovementDirection()
+    {
+        return _movementDirection.normalized;
+    }
+
     private void Awake()
     {
+        _stateController = GetComponent<StateController>();
         _playerRigidbody = GetComponent<Rigidbody>();
         _playerRigidbody.freezeRotation = true;
         _playerRigidbody.interpolation = RigidbodyInterpolation.Interpolate;
@@ -130,6 +167,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         SetInputs();
+        SetStates();
         SetPlayerDrag();
         LimitPlayerSpeed();
     }
---
+++ Assets/_GameAssets/Scripts/Gameplay/Player/StateController.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class StateController : MonoBehaviour
+{
+   private PlayerState _currentPlayerState = PlayerState.Idle;
+
+   public void ChangeState(PlayerState newPlayerState)
+   {
+      if (_currentPlayerState != newPlayerState)
+      {
+         return;
+      }
+      _currentPlayerState = newPlayerState;
+      
+   }
+
+   public PlayerState GetCurrentState()
+   {
+      return _currentPlayerState;
+   }
+
+   private void Start()
+   {
+      ChangeState(PlayerState.Idle);
+   }
+}
